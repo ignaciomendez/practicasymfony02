4ec7fe36cd690a08aa12ae08502a7dc80ec830d3
Nachito <ignaciofmz@gmail.com>
2016-08-07 22:34:43 -0300

    first page

---
+++ practica02/README.md
@@ -0,0 +1,4 @@
+practica02
+==========
+
+A Symfony project created on August 7, 2016, 9:35 pm.
---
+++ practica02/src/AppBundle/Controller/GenusController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace AppBundle\Controller;
+
+class GenusController
+{
+	/**
+	* @Route("/genus")
+	*/
+	public funcion showAction()
+	{
+		return new Response('Under the Sea!');
+	}
+}
+
+?>
